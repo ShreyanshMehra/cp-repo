42de76b0c5f1b056fe9ddee5d1c7c518bdbc0489
ShreyanshMehra <99526135+ShreyanshMehra@users.noreply.github.com>
2024-10-06 01:36:43 +0530

    900(little bit thinking)
    dumb enough to make cases but you can always put yo yo in opposite corners

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+int opp_corner(int x,int y,int n,int m){
+    if(x==y && x==1){
+
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int m,n,x_i,y_i;
+        int x1,y1,x2,y2;
+        cin>>m>>n>>x_i>>y_i;
+        cout<<1<<" "<<1<<" "<<m<<" "<<n<<endl;
+        // if(m==1 && n==1)cout<<1<<" "<<1<<" "<<1<<" "<<1<<endl;
+        // else if(x_i==y_i &&x_i==1){
+        //     x1=m;y1=n;
+        //     if(m>1){
+        //         x2=x_i+1;
+        //         y2=y_i;
+        //     }
+        //     else{
+        //         y2=y_i+1;
+        //         x2=x_i;
+        //     }
+        // }
+        // else if(y_i==n && x_i==m){
+        //     x1=1;y1=1;
+        //     if(m>1){
+        //         x2=x_i-1;
+        //         y2=y_i;
+        //     }
+        //     else{
+        //         y2=y_i-1;
+        //         x2=x_i;
+        //     }
+        // }
+        // else if(x_i==1 && y_i==n || x_i==m && y_i==1){
+        //     x1=y_i;
+        //     y1=x_i;
+        //     if(x_i==1){
+        //         x2=x_i-1;
+        //         y2=y_i;
+        //     }
+        //     if(y_i==1){
+        //         y2=y_i+1;
+        //         x2=x_i;
+        //     }
+        // }
+        // else{
+        //     x1=1;
+        //     y1=1;
+        //     x2=m;
+        //     y2=n;
+        // }
+        // cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2<<endl;
+
+    }
+}
