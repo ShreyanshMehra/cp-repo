28d5b36aaccb08abb7bbb86cd2c2dbcb5dda8985
ShreyanshMehra <99526135+ShreyanshMehra@users.noreply.github.com>
2024-10-18 14:01:39 +0530

    1000(formula)

---
+++ A_Buying_Torches.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long x,y,k;
+        cin>>x>>y>>k;
+        long long total_sticks = (y+1)*k;
+        long long trades=k;
+        long long sticks = x-1;
+        if((total_sticks-1)%sticks==0){
+            trades+=((total_sticks-1)/sticks);
+        }
+        else{
+            trades+=(total_sticks/sticks)+1;
+        }
+        cout<<trades<<endl;
+    }
+}
